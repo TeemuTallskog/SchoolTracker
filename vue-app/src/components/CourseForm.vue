9b5be5ecb961f68cbe843f038516e108f04877fd
Sylvester Salo <sylttysalo@gmail.com>
2021-11-29 15:57:57 +0200

    Improved form and table
    Form information shown in table. Table items can be deleted.

--- vue-app/src/components/CourseForm.vue
+++ vue-app/src/components/CourseForm.vue
@@ -1,11 +1,31 @@
 <template>
 <div id="course-form">
   <form @submit.prevent="handleSubmit">
-    <label>Kurssin nimi:</label>
-    <input v-model="course.name" type="text">
-    <label>Linkki:</label>
-    <input v-model="course.link" type="text">
-    <button>Luo kurssi</button>
+    <h1>Uusi tehtävä</h1>
+
+    <label for="name"></label>
+    <input id="name" v-model="course.name" type="text" placeholder="Kurssin nimi">
+
+    <label for="link"></label>
+    <input id ="link" v-model="course.link" type="text" placeholder="Linkki">
+
+    <br>
+
+    <label for="date"></label>
+    <input id="date" v-model="course.date" type="date">
+
+
+    <label for="taskName"></label>
+    <input id ="taskName" v-model="course.taskName" type="text" placeholder="Tehtävän nimi">
+
+    <br>
+
+    <textarea id="taskInfo" rows="10" cols="56" v-model="course.taskInfo" type="text" placeholder="Lisätietoja tehtävästä"></textarea>
+
+    <br>
+
+    <button>Luo Tehtävä</button>
+
   </form>
 </div>
 </template>
@@ -18,6 +38,9 @@ export default {
       course: {
         name: '',
         link: '',
+        date:'',
+        taskName: '',
+        taskInfo:'',
       },
     }
   },
@@ -30,5 +53,20 @@ export default {
 </script>
 
 <style scoped>
+
+div {
+  padding: 10px;
+  border: 1px solid;
+  width: 700px;
+}
+
+input {
+  width: 200px;
+ margin: 10px;
+  align: bottom;
+}
+
+
+
 
 </style>
